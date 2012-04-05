8d9006f5e68ab42d0c9cc32faab3576bc3845e69
Frederic Morel <slerinflor@gmail.com>
2012-04-05 19:27:09 -0400

    Added David's logo

--- App Domain/Login.Designer.cs
+++ App Domain/Login.Designer.cs
@@ -35,12 +35,15 @@
 			this.txtPass = new System.Windows.Forms.TextBox();
 			this.bLogin = new System.Windows.Forms.Button();
 			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// lbUser
 			// 
+			this.lbUser.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
 			this.lbUser.AutoSize = true;
-			this.lbUser.Location = new System.Drawing.Point(12, 15);
+			this.lbUser.Location = new System.Drawing.Point(105, 210);
 			this.lbUser.Name = "lbUser";
 			this.lbUser.Size = new System.Drawing.Size(55, 13);
 			this.lbUser.TabIndex = 0;
@@ -48,8 +51,9 @@
 			// 
 			// lbPass
 			// 
+			this.lbPass.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
 			this.lbPass.AutoSize = true;
-			this.lbPass.Location = new System.Drawing.Point(12, 41);
+			this.lbPass.Location = new System.Drawing.Point(381, 210);
 			this.lbPass.Name = "lbPass";
 			this.lbPass.Size = new System.Drawing.Size(53, 13);
 			this.lbPass.TabIndex = 1;
@@ -57,23 +61,25 @@
 			// 
 			// txtUser
 			// 
-			this.txtUser.Location = new System.Drawing.Point(73, 12);
+			this.txtUser.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.txtUser.Location = new System.Drawing.Point(166, 207);
 			this.txtUser.Name = "txtUser";
-			this.txtUser.Size = new System.Drawing.Size(199, 20);
+			this.txtUser.Size = new System.Drawing.Size(209, 20);
 			this.txtUser.TabIndex = 2;
 			// 
 			// txtPass
 			// 
-			this.txtPass.Location = new System.Drawing.Point(73, 38);
+			this.txtPass.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.txtPass.Location = new System.Drawing.Point(442, 207);
 			this.txtPass.Name = "txtPass";
 			this.txtPass.PasswordChar = '*';
-			this.txtPass.Size = new System.Drawing.Size(199, 20);
+			this.txtPass.Size = new System.Drawing.Size(209, 20);
 			this.txtPass.TabIndex = 3;
 			// 
 			// bLogin
 			// 
 			this.bLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bLogin.Location = new System.Drawing.Point(176, 74);
+			this.bLogin.Location = new System.Drawing.Point(423, 237);
 			this.bLogin.Name = "bLogin";
 			this.bLogin.Size = new System.Drawing.Size(98, 23);
 			this.bLogin.TabIndex = 4;
@@ -83,7 +89,8 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(13, 74);
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Location = new System.Drawing.Point(254, 237);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(139, 23);
 			this.button1.TabIndex = 5;
@@ -91,12 +98,22 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(736, 183);
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
 			// Login
 			// 
 			this.AcceptButton = this.bLogin;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(286, 107);
+			this.ClientSize = new System.Drawing.Size(758, 270);
+			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.bLogin);
 			this.Controls.Add(this.txtPass);
@@ -111,6 +128,7 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Log in to Fastbooks";
 			this.Load += new System.EventHandler(this.Login_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -124,5 +142,6 @@
         private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.Button bLogin;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
